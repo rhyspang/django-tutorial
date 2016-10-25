c8caa72fafe882ea5b1857727795be59f5253b33
stoonejames <stoonejames@gmail.com>
2016-10-25 15:57:26 +0800

    part5 finished

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
-
-
+from django.utils import timezone
 
 
 # def index(request):
@@ -25,21 +24,28 @@ from .models import Choice, Question
 # 	return render(request, 'polls/results.html', {'question': question})
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
-	model = Question
-	template_name = 'polls/results.html'
+    model = Question
+    template_name = 'polls/results.html'
 
-	
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
