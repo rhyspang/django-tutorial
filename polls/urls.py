897caf2136a1867ef2164a51701a2f36cdde67c1
stoonejames <stoonejames@gmail.com>
2016-10-25 14:12:48 +0800

    part4 finished

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@
 # @Author: rhys
 # @Date:   2016-10-24 12:13:46
 # @Last Modified by:   stoonejames
-# @Last Modified time: 2016-10-24 18:38:50
+# @Last Modified time: 2016-10-25 13:49:24
 
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -11,8 +11,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='result'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
